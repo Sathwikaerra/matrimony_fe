b949c7422f102c1e6704837af4b2611584cfd5b2
Sathwikaerra <b192624@rgukt.ac.in>
2026-05-09 23:15:31 +0530

    ggg

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+import { StrictMode,useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './redux/store'
 import './index.css'
 import App from './App.jsx'
+
+import { registerServiceWorker } from "./services/notificationService";
+
+useEffect(() => {
+   registerServiceWorker();
+}, []);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
